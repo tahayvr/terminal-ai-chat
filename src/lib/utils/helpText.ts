e926ae2a3eb40718aa9b7a74faeccfbfd7b56849
Taha Nejad <paprikataha@gmail.com>
2025-05-24 13:17:07 -0700

    style: change color of help text and add minor info

--- src/lib/utils/helpText.ts
+++ src/lib/utils/helpText.ts
@@ -1,22 +1,24 @@
 export const helpText = `<span class="text-yellow-500">Terminal Chat LLM</span>
 <div class="flex flex-col text-xs text-gray-500 mb-2">
-    <span>by <a href="https://taha.gg" class="text-zinc-500">Taha aka noiseRandom</a></span>
-    <span>Version: 1.0.0</span> 
-    <span>Commands:</span>
+    <span>by <a href="https://taha.gg" class="text-zinc-500">noiseRandom</a></span>
+    <span>Source code --> <a href="https://github.com/tahayvr/terminal-chat-llm" class="text-zinc-500">GitHub</a></span>
+    <span>Model --> Gemma3 1B</span>
+    <span>Version: 1.0.1</span> 
 </div>
-        <div class="command-list">
-  <div class="command-item flex gap-2">
-    <span class="text-green-400 font-bold">/help</span>
+    <span class="text-gray-500">--Commands--</span>
+    <div class="command-list">
+    <div class="command-item flex gap-2">
+    <span class="text-green-500 font-bold">/help</span>
     <div>-- displays help message</div>
   </div>
   
   <div class="command-item  flex gap-2">
-    <span class="text-green-400 font-bold">/clear</span>
+    <span class="text-green-500 font-bold">/clear</span>
     <div>-- clears chat history</div>
   </div>
   
   <div class="command-item  flex gap-2">
-    <span class="text-green-400 font-bold">/export</span>
+    <span class="text-green-500 font-bold">/export</span>
     <div>-- exports full conversation as markdown file</div>
   </div>
 </div>`;
